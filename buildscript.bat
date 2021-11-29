b74e1572a33bc38deff2800d2b00eb6d498428c3
甄镭 <zhenlei1970@hotmail.com>
2021-11-29 16:24:40 +0800

    improve buildscript

--- buildscript.bat
+++ buildscript.bat
@@ -1,12 +1,3 @@
 dotnet publish ./src/IDS4Admin.Admin/IDS4Admin.Admin.csproj /p:Configuration=Release /p:PublishProfile=FolderProfile
-dotnet publish ./src/IDS4Admin.Admin/IDS4Admin.Admin.csproj /p:Configuration=Release /p:PublishProfile=FolderProfile1
-dotnet publish ./src/IDS4Admin.Admin/IDS4Admin.Admin.csproj /p:Configuration=Release /p:PublishProfile=FolderProfile2
-dotnet publish ./src/IDS4Admin.Admin/IDS4Admin.Admin.csproj /p:Configuration=Release /p:PublishProfile=FolderProfile3
-
 dotnet publish ./src/IDS4Admin.STS.Identity/IDS4Admin.STS.Identity.csproj /p:Configuration=Release /p:PublishProfile=FolderProfile
-dotnet publish ./src/IDS4Admin.STS.Identity/IDS4Admin.STS.Identity.csproj /p:Configuration=Release /p:PublishProfile=FolderProfile1
-dotnet publish ./src/IDS4Admin.STS.Identity/IDS4Admin.STS.Identity.csproj /p:Configuration=Release /p:PublishProfile=FolderProfile2
-dotnet publish ./src/IDS4Admin.STS.Identity/IDS4Admin.STS.Identity.csproj /p:Configuration=Release /p:PublishProfile=FolderProfile3
-
 dotnet publish ./src/IDS4Admin.Tool/IDS4Admin.Tool.csproj /p:Configuration=Release /p:PublishProfile=FolderProfile
-dotnet publish ./src/IDS4Admin.Tool/IDS4Admin.Tool.csproj /p:Configuration=Release /p:PublishProfile=FolderProfile1
